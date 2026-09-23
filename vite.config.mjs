9070d76dcfadbb0412e87ce81ae05e6935c396c6
Makkai Gábor <makkaigabor01@gmail.com>
2026-09-23 15:01:25 +0200

    Successfully fix build and configure project for Vercel

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import injectHTML from 'vite-plugin-html-inject'
 
 export default defineConfig({
-  plugins: [injectHTML()],
   build: {
     rollupOptions: {
       input: {
